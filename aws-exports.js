dc4506431aedde59a36719475493ddfc5046a748
Vayalpati Koushik Reddy <vayalpatikoushikreddy@Vayalpatis-Air.lan>
2025-04-03 00:32:41 -0400

    workign without chat

--- aws-exports.js
+++ aws-exports.js
@@ -1,19 +1,19 @@
 const awsmobile = {
     Auth: {
         // REQUIRED - Amazon Cognito Identity Pool ID
-        identityPoolId: 'us-east-2:78b5564e-3125-4350-9af5-e0a4f353b439', // e.g. 'us-east-1:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+        identityPoolId: 'us-east-2:a42d06ce-29f4-4ddb-a516-e93311fbbcde', // e.g. 'us-east-1:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
         
         // REQUIRED - Amazon Cognito Region
         region: 'us-east-2', // e.g., 'us-east-1'
         
         // OPTIONAL - Amazon Cognito User Pool ID
-        userPoolId: 'us-east-2_4iJK5Y8Zz', // e.g. 'us-east-1_xxxxxxxx'
+        userPoolId: 'us-east-2_NFqhqiTLZ', // e.g. 'us-east-1_xxxxxxxx'
         
         // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-        userPoolWebClientId: '28k0tme8iedpt2bfikkauij1ec', // e.g. 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
+        userPoolWebClientId: '5gs6g6t5mtscmlfoe0767mpdo4', // e.g. 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
         
         mandatorySignIn: true,
-    }
+    },
 };
 
 export default awsmobile; 
